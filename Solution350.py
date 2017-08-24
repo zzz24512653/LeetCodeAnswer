38425195dbb9a0af04af05fcabd8b96d2036cfb7
zhongzhengzhang <zzz24512653@gmailcom>
2017-08-24 11:09:01 +0800

    add solution 350

--- Solution350.py
+++ Solution350.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution(object):
     def binary_search(self, nums, i, j, target):
         if j < i:
@@ -10,15 +13,22 @@ class Solution(object):
         else:
             return self.binary_search(nums, mid+1, j, target)
 
-    def intersection(self, nums1, nums2):
+    def intersect(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
         """
+        counter1 = defaultdict(lambda: 0)
+        for n in nums1:
+            counter1[n] += 1
+        counter2 = defaultdict(lambda: 0)
+        for n in nums2:
+            counter2[n] += 1
+
         nums = sorted(list(set(nums1)))
         ret = []
         for n in set(nums2):
             if self.binary_search(nums, 0, len(nums)-1, n) != -1:
-                ret.append(n)
+                ret.extend([n] * min(counter1[n], counter2[n]))
         return ret
